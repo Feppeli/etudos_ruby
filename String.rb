285702149c2986e59d3e25cb5b84b43b04be851c
Douglas felipe <dfsilvat@gmail.com>
2022-10-05 14:31:56 -0300

    String

--- String.rb
+++ String.rb
@@ -8,5 +8,22 @@ puts string_especial_usando_aspas_simples
 string_especial_usando_aspas_duplas = "isso é \"normal\" e 'util' no mundo ruby" # => isso é "normal" e 'util' no mundo ruby
 puts string_especial_usando_aspas_duplas
 
-string_especial = %{isso é "normal" e 'util' no mundo ruby}
+
+=begin
+Qual quer caractere alfanumerico pode ser utilizado após a porcentagem por exemplo:
+
+string_especial = %[Isso é "normal" e 'util' no mundo Ruby]
+puts string_especial
+# => "Isso é \"normal\" e 'util' em Ruby"
+
+string_especial = %?Isso é "normal" e 'util' no mundo Ruby?
+puts string_especial
+# => "Isso é \"normal\" e 'util' em Ruby"
+
+string_especial = %~Isso é "normal" e 'util' no mundo Ruby~
+puts string_especial
+# => "Isso é \"normal\" e 'util' em Ruby"
+
+=end
+string_especial = %{isso é "normal" e 'util' no mundo ruby} # => isso é "normal" e 'util' no mundo ruby
 puts string_especial
