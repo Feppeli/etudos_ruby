f59e19b9f1d23331ff3637a1a54b8f4d67318695
Douglas felipe <dfsilvat@gmail.com>
2022-10-05 13:12:52 -0300

    For_while_Until

---
+++ For_While_Until.rb
@@ -0,0 +1,13 @@
+### FOR
+
+for num in (1..100) # Dessa Maneira selecionamos todos os numeros de 1 a 100
+    puts "Num: #{num}"
+end
+
+### WHILE
+
+numero = 0 # Definimos o valor de numero
+while numero <= 100 # Enquanto o numero for menor ou igual a 100 faça isso
+    puts "Numero: #{numero}" # Imprima no console "Numero (e o valor atual que iniciará em 0)"
+    numero += 1 # Após imprimir o valor de numero incrementará um novo valor a variavel numero 
+end
